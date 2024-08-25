1eaa452538c8710bcabbd4ef0e0bfb7e365ab28a
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-08-26 02:29:14 +0800

    Set pages routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,47 @@ import "./index.css";
 
 import Layout from "./layout";
 
+import Dashboard from "./pages/dashboard";
+
+import Assignment from "./pages/assignment";
+import { loader as assignmentLoader } from "./pages/assignment/utils";
+
+import AssignmentCards from "./pages/assignment/cards";
+
+import Course from "./pages/course";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        index: true,
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/assignment",
+        loader: assignmentLoader,
+        element: <Assignment />,
+        children: [
+          {
+            path: ":id",
+            element: <AssignmentCards />,
+          },
+        ],
+      },
+      {
+        path: "/course",
+        element: <Course />,
+        children: [
+          {
+            path: ":id",
+            element: <Course />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
